83040d7ec4b07a8d62a2d4edd406886528eecf5e
sakthi <sakthiece08@gmail.com>
2023-10-18 11:28:13 -0400

    Code modularized

--- spring-security/src/main/java/com/teqmonic/springsecurity/configuration/CustomSecurityConfiguration.java
+++ spring-security/src/main/java/com/teqmonic/springsecurity/configuration/CustomSecurityConfiguration.java
@@ -5,20 +5,12 @@ package com.teqmonic.springsecurity.configuration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,6 +22,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class CustomSecurityConfiguration {
 
+	
+	@Bean
+	InMemoryUserDetailsManager users() {
+		// below password details can be passed from externally as well, so clear text credentials can be hided in the source code
+		UserDetails user = User.builder().username("user").password("{noop}password").roles("USER").build();
+		UserDetails admin = User.builder().username("admin").password("{noop}password").roles("USER", "ADMIN").build();
+		return new InMemoryUserDetailsManager(user, admin);
+	}
 
     /**
      * /api/admin --> should have ROLE ADMIN
@@ -49,33 +49,6 @@ public class CustomSecurityConfiguration {
 			.formLogin(withDefaults());
 
 		return http.build(); 	
-	}
-
-    @Bean
-    DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService());
-        return provider;
-        
-    }
-	
-	@Bean
-    UserDetailsService userDetailsService() {
-		// below password details can be passed from externally as well, so clear text credentials can be hided in the source code
-		UserDetails user = User.builder().username("user").password("{noop}password").roles("USER").build();
-		UserDetails admin = User.builder().username("admin").password("{noop}password").roles("USER", "ADMIN").build();
-		return new InMemoryUserDetailsManager(user, admin);
-	}
-	
-
-	@SuppressWarnings("deprecation")
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
-		// encoders.put("bcrypt", new BCryptPasswordEncoder());
-		encoders.put("noop", NoOpPasswordEncoder.getInstance()); // only for testing purpose
-		return new DelegatingPasswordEncoder("noop", encoders);
 	}
 
 }
